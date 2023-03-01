ffd48610bce1a707f9452748f352f6cbf34dc87e
Martin Flores <mfloresg@gmail.com>
2023-02-28 22:23:55 -0600

    codigo inicial para tarea 2

--- weather-report/src/test/java/tec/bd/openweather/EnvironmentVariableAPIKeyProviderTest.java
+++ weather-report/src/test/java/tec/bd/openweather/EnvironmentVariableAPIKeyProviderTest.java
@@ -1,19 +1,26 @@
 package tec.bd.openweather;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ExtendWith(SystemStubsExtension.class)
 public class EnvironmentVariableAPIKeyProviderTest {
 
-    @Ignore
+    @SystemStub
+    private EnvironmentVariables environmentVariables;
+
     @Test
     public void GivenAPIKeyRequested_WhenAPIKeyAsEnvironmentVariable_ThenReturn() {
+        environmentVariables.set("OW_API_KEY", "the-api-key");
 
         var apiKeyProvider = new EnvironmentVariableAPIKeyProvider();
         var actual = apiKeyProvider.getAPIKey();
 
-        assertThat(actual).isNotNull();
+        assertThat(actual).isEqualTo("the-api-key");
     }
 }
--- weather-report/src/test/java/tec/bd/weather/storage/InMemoryWeatherReportStorageTest.java
+++ weather-report/src/test/java/tec/bd/weather/storage/InMemoryWeatherReportStorageTest.java
@@ -1,30 +1,41 @@
 package tec.bd.weather.storage;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryWeatherReportStorageTest {
 
+    /*
+    Este test case se tiene que implementar
+     */
+
+
+    @Disabled
     @Test
     public void findReport() {
         fail("Needs to be implemented");
     }
 
+    @Disabled
     @Test
     public void findAllReports() {
         fail("Needs to be implemented");
     }
 
+    @Disabled
     @Test
     public void saveReport() {
         fail("Needs to be implemented");
     }
 
+    @Disabled
     @Test
     public void updateReport() {
         fail("Needs to be implemented");
     }
 
+    @Disabled
     @Test
     public void deleteReport() {
         fail("Needs to be implemented");
